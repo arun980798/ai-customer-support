2186aba68f249726f79f2a8adde9490a365463f6
Pankaj <pankajpenu147@gmail.com>
2026-05-02 22:29:15 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,33 @@
 import express from 'express';
-import authRouter from './routes/auth.routes';
-
+import authRouter from './routes/auth.routes.js';
+import {Strategy as GoogleStrategy} from 'passport-google-oauth20'
+import { config } from './config/config.js'
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import passport from 'passport';
 
 
 const app = express();
 
-
-
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(cookieParser())
 
 app.use("/api/auth",authRouter);
 
 app.get('/', (req, res) => {
   res.send('server is running');
 });
+
+app.use(passport.initialize())
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/api/auth/google/callback"
+},( accessToken,refreshToken,profile,done)=>{
+    return done(null,profile);
+    
+}))
 
 export default app;
