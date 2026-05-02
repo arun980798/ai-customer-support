a241d7b0f2c38a6372b2cc8253413e0e38b6578a
Pankaj <pankajpenu147@gmail.com>
2026-05-02 22:26:13 +0530

    Update MongoDB connection log message

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB connected succcessfully`);
 };
 
 export default connectDB;
