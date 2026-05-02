1f0b801ece9c36a16f498c1a1e87d3c6071b3081
Pankaj <pankajpenu147@gmail.com>
2026-05-02 22:28:29 +0530

    Update authentication routes to include Google OAuth

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
-import { register, verifyEmail, login, getMe } from "../controllers/auth.controller.js";
+import { register, login, googleCallback, getCompanies } from "../controllers/auth.controller.js";
 import { registerValidator, loginValidator } from "../validators/auth.validator.js";
-import { authUser } from "../middleware/auth.middleware.js";
+import passport from "passport";
+import { authenticateAdmin } from "../middleware/auth.middleware.js";
 
 const authRouter = Router();
 
-authRouter.post("/register", registerValidator, register);
+authRouter.post("/register", authenticateAdmin, registerValidator, register);
+authRouter.post("/login", loginValidator, login);
+authRouter.get('/getcompanies',getCompanies)
 
-
-authRouter.post("/login", loginValidator, login)
-
-
+// Google OAuth routes
+authRouter.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+authRouter.get('/google/callback', passport.authenticate('google', { session: false, failureRedirect: '/' }), googleCallback);
 
 export default authRouter;
